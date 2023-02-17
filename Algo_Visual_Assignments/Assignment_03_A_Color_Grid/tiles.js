b6116f8989203a917eaed278a4a78be34c26bda5
Aryan Raval <2533aryan@gmail.com>
2023-02-17 17:05:36 -0500

    edited

--- Algo_Visual_Assignments/Assignment_03_A_Color_Grid/tiles.js
+++ Algo_Visual_Assignments/Assignment_03_A_Color_Grid/tiles.js
@@ -38,3 +38,46 @@
 //     drawTiles();
 //   };
   
+function drawTiles() {
+  const gridSize = 10;
+  const tileSize = 50; // pixels
+  const tileSpacing = 2; // pixels
+  const container = document.querySelector('#grid-container');
+  
+  // Generate and append tiles to the container
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    container.appendChild(tile);
+  }
+  
+  // Style the tiles
+  const tiles = document.querySelectorAll('.tile');
+  for (let i = 0; i < tiles.length; i++) {
+    const color = generateTileColor(i, gridSize);
+    tiles[i].style.backgroundColor = color;
+    tiles[i].style.width = tileSize + 'px';
+    tiles[i].style.height = tileSize + 'px';
+    tiles[i].style.margin = tileSpacing + 'px';
+  }
+  
+  // Add mouse hover effect using CSS
+  container.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('tile')) {
+      event.target.classList.add('tile-hover');
+    }
+  });
+  
+  container.addEventListener('mouseout', (event) => {
+    if (event.target.classList.contains('tile')) {
+      event.target.classList.remove('tile-hover');
+    }
+  });
+}
+
+function generateTileColor(tileIndex, gridSize) {
+  const hue = tileIndex % gridSize / gridSize * 360;
+  const saturation = 50 + Math.floor(tileIndex / gridSize) / gridSize * 50;
+  const lightness = 50 + Math.floor(tileIndex / gridSize) / gridSize * 50;
+  return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+}
