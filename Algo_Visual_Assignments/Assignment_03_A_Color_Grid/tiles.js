9266953264e496303d8a95acbc5e3364ad7ab90b
Aryan Raval <2533aryan@gmail.com>
2023-02-17 19:51:02 -0500

    color done

--- Algo_Visual_Assignments/Assignment_03_A_Color_Grid/tiles.js
+++ Algo_Visual_Assignments/Assignment_03_A_Color_Grid/tiles.js
@@ -9,9 +9,9 @@ function drawTiles() {
             tile.classList.add('tile');
 
             // Calculate the HSL values for the tile based on its position
-            var hue = 240 - (row * 24);
-            var saturation = 100 - (col * 10);
-            var lightness = 50 + (row * 5) + (col * 2);
+            var hue = 260 - (row * 10) + (col*10);
+            var saturation = 250 - (col * 10);
+            var lightness = 30 + (row * 5) + (col * 2);
 
             // Set the background color of the tile
             tile.style.backgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
