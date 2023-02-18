a88d814b8be55dea87951f136315faa45112c9d2
Aryan Raval <2533aryan@gmail.com>
2023-02-17 19:27:49 -0500

    more progress

--- Algo_Visual_Assignments/Assignment_03_A_Color_Grid/tiles.js
+++ Algo_Visual_Assignments/Assignment_03_A_Color_Grid/tiles.js
@@ -1,6 +1,13 @@
 function generateRandomColor() {
-	// This function generates a random hex color code
-	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+	// Generate a random hue between 0 and 360 degrees
+	let hue = Math.floor(Math.random() * 360);
+
+	// Generate a random saturation and lightness between 50% and 80%
+	let saturation = Math.floor(Math.random() * 30) + 50;
+	let lightness = Math.floor(Math.random() * 30) + 50;
+
+	// Return the color in HSL format
+	return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 }
 
 function drawTiles() {
