46ff257ca7d241d813d57e064883edb05527a0c9
Aryan Raval <2533aryan@gmail.com>
2023-02-17 19:40:57 -0500

    more progress

--- Algo_Visual_Assignments/Assignment_03_A_Color_Grid/tiles.js
+++ Algo_Visual_Assignments/Assignment_03_A_Color_Grid/tiles.js
@@ -1,24 +1,27 @@
-function generateRandomColor() {
-	// Generate a random hue between 0 and 360 degrees
-	let hue = Math.floor(Math.random() * 360);
-
-	// Generate a random saturation and lightness between 50% and 80%
-	let saturation = Math.floor(Math.random() * 30) + 50;
-	let lightness = Math.floor(Math.random() * 30) + 50;
-
-	// Return the color in HSL format
-	return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-}
-
 function drawTiles() {
-	var gridContainer = document.getElementById('grid-container');
-	for (var i = 0; i < 100; i++) {
-		var tile = document.createElement('div');
-		tile.classList.add('tile');
-		tile.style.backgroundColor = generateRandomColor();
-		gridContainer.appendChild(tile);
-	}
+    // Get a reference to the container element for the tiles
+    var gridContainer = document.getElementById('grid-container');
+
+    // Generate and append the tiles to the container
+    for (var row = 0; row < 10; row++) {
+        for (var col = 0; col < 10; col++) {
+            var tile = document.createElement('div');
+            tile.classList.add('tile');
+
+            // Calculate the HSL values for the tile based on its position
+            var hue = 240 - (row * 24);
+            var saturation = 100 - (col * 10);
+            var lightness = 50 + (row * 5) + (col * 2);
+
+            // Set the background color of the tile
+            tile.style.backgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+
+            // Append the tile to the container
+            gridContainer.appendChild(tile);
+        }
+    }
 }
+
 
 window.onload = function() {
 	drawTiles();
