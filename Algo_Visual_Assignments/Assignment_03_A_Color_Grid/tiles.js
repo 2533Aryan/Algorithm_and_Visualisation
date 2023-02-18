1d9ce057e06988a6c0ac7e88d65e8ae30bb80343
Aryan Raval <2533aryan@gmail.com>
2023-02-17 20:21:35 -0500

    DONE!

--- Algo_Visual_Assignments/Assignment_03_A_Color_Grid/tiles.js
+++ Algo_Visual_Assignments/Assignment_03_A_Color_Grid/tiles.js
@@ -1,14 +1,14 @@
 function drawTiles() {
-    // Get a reference to the container element for the tiles
+    // Get a reference to the container element for tiles
     var gridContainer = document.getElementById('grid-container');
 
-    // Generate and append the tiles to the container
+    // Generate tiles
     for (var row = 0; row < 10; row++) {
         for (var col = 0; col < 10; col++) {
             var tile = document.createElement('div');
             tile.classList.add('tile');
 
-            // Calculate the HSL values for the tile based on its position
+            // Calculate the HSL values based on tile's position
             var hue = 260 - (row * 10) + (col*10);
             var saturation = 250 - (col * 10);
             var lightness = 30 + (row * 5) + (col * 2);
@@ -16,9 +16,9 @@ function drawTiles() {
             // Set the background color of the tile
             tile.style.backgroundColor = 'hsl(' + hue + ', ' + saturation + '%, ' + lightness + '%)';
 
-			// Add an event listener to the tile that changes its color permanently when clicked
+			// Event listener that changes tile's color when clicked
 			tile.addEventListener('click', function() {
-			this.style.backgroundColor = 'black';
+				this.style.backgroundColor = 'black';
 			});
 
             // Append the tile to the container
