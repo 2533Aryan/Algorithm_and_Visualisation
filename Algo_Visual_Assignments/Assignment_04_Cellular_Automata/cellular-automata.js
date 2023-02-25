e77a18c3450bb260f5e418a514727533bfa0f1ec
Aryan Raval <2533aryan@gmail.com>
2023-02-24 21:11:17 -0500

    js

--- Algo_Visual_Assignments/Assignment_04_Cellular_Automata/cellular-automata.js
+++ Algo_Visual_Assignments/Assignment_04_Cellular_Automata/cellular-automata.js
@@ -27,47 +27,43 @@ function drawRuleBox() {
     ruleBox.style.backgroundColor = "white";
     
     for (let j = 7; j >= 0; j--) {
-	
-	let ruleContainer = document.createElement('div');
-	ruleContainer.classList.add('float-container');
-	ruleContainer.style.width = 3 * TILE_SIZE + 'px';
-	ruleContainer.style.height = 2 * TILE_SIZE + 'px';
+		let ruleContainer = document.createElement('div');
+		ruleContainer.classList.add('float-container');
+		ruleContainer.style.width = 3 * TILE_SIZE + 'px';
+		ruleContainer.style.height = 2 * TILE_SIZE + 'px';
 
-	for (let i = 0; i < 4; i++) {
-	    let tile = document.createElement('div')
-	    tile.classList.add('rule-tile');
-	    tile.style.left = i * TILE_SIZE + 'px';
-	    tile.style.top = '0px';
-	    tile.style.width = TILE_INNER + 'px';
-	    tile.style.height = TILE_INNER + 'px';
+		for (let i = 0; i < 4; i++) {
+			let tile = document.createElement('div')
+			tile.classList.add('rule-tile');
+			tile.style.left = i * TILE_SIZE + 'px';
+			tile.style.top = '0px';
+			tile.style.width = TILE_INNER + 'px';
+			tile.style.height = TILE_INNER + 'px';
 
-	    if (i < 3) {
-		let bit = Math.floor(j / Math.pow(2, 2-i)) % 2;
-		if (bit == 0) {
-		    tile.style.backgroundColor = 'white';
-		} else {
-		    tile.style.backgroundColor = 'black';		    
+			if (i < 3) {
+				let bit = Math.floor(j / Math.pow(2, 2-i)) % 2;
+			if (bit == 0) {
+				tile.style.backgroundColor = 'white';
+			} else {
+				tile.style.backgroundColor = 'black';		    
+			}
+			}
+			
+
+			if (i == 3) {
+				tile.style.left = TILE_SIZE + 'px';
+				tile.style.top = TILE_SIZE + 'px';
+				tile.style.backgroundColor = 'white';
+				tile.addEventListener("click", tileUpdate);
+			}
+			ruleContainer.appendChild(tile);
 		}
-	    }
-	    
 
-	    if (i == 3) {
-		tile.style.left = TILE_SIZE + 'px';
-		tile.style.top = TILE_SIZE + 'px';
-		tile.style.backgroundColor = 'white';
-		tile.addEventListener("click", tileUpdate);
-	    }
-	    ruleContainer.appendChild(tile);
+		ruleBox.appendChild(ruleContainer);
 	}
-
-	ruleBox.appendChild(ruleContainer);
-    }
-
-    
 }
 
 function drawGrid() {
-
     // make the container the right size
     const container = document.querySelector('#grid-background');
     container.style.width = CONTAINER_WIDTH + "px";
