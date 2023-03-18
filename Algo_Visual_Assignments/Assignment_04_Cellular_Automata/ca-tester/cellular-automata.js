df2e143b6112f835bd0de78ec4eaa5d18741cb7f
Aryan Raval <2533aryan@gmail.com>
2023-03-17 20:27:38 -0400

    checking if config is array

--- Algo_Visual_Assignments/Assignment_04_Cellular_Automata/ca-tester/cellular-automata.js
+++ Algo_Visual_Assignments/Assignment_04_Cellular_Automata/ca-tester/cellular-automata.js
@@ -1,5 +1,9 @@
 // const { config } = require("process");
 
 function applyRule(config, rule) {
+    // check if the input 'config' is an array containing only 0 or 1
+    if(config.isArray || config.every() == 0 && config.every() == 1)
+    
+    
     module.exports = { applyRule };
 }
