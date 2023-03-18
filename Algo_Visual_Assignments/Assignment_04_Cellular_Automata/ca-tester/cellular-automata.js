d5b298776b38d0a5cfac3048dc2c2c0f716b2b4c
Aryan Raval <2533aryan@gmail.com>
2023-03-17 20:33:26 -0400

    array checked

--- Algo_Visual_Assignments/Assignment_04_Cellular_Automata/ca-tester/cellular-automata.js
+++ Algo_Visual_Assignments/Assignment_04_Cellular_Automata/ca-tester/cellular-automata.js
@@ -2,8 +2,23 @@
 
 function applyRule(config, rule) {
     // check if the input 'config' is an array containing only 0 or 1
-    if(config.isArray || config.every() == 0 && config.every() == 1)
+
+    // if(config.isArray || config.every() == 0 && config.every() == 1)
+    if(Array.isArray(config)) {
+        console.log("is Array")
+    }
     
-    
+    console.log("Fail")
+
+
+
     module.exports = { applyRule };
 }
+
+const testConfig = [0,1,0,0,1,1,0,0]
+const testRule = 50
+
+const testConfig2 = "string"
+const testRule2 = 350
+
+applyRule(testConfig2, testRule)
