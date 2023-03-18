26fb861ce9e4c4233455c467826c039d6d713481
Aryan Raval <2533aryan@gmail.com>
2023-03-17 23:25:11 -0400

    Rule applied

--- Algo_Visual_Assignments/Assignment_04_Cellular_Automata/ca-tester/cellular-automata.js
+++ Algo_Visual_Assignments/Assignment_04_Cellular_Automata/ca-tester/cellular-automata.js
@@ -1,28 +1,38 @@
-// const { config } = require("process");
-
 function applyRule(config, rule) {
 
-    // check if the input 'config' is an array containing only 0 or 1
+    // Check if the input 'config' is an array containing only 0 or 1
     if(!Array.isArray(config) || !config.every(x => x === 0 || x === 1)) {
         throw new console.error("Invalid Input Configuration");
     }
 
-    // check if the input 'rule' is an integer in range (0,255)
+    // Check if the input 'rule' is an integer in range (0,255)
     if (!Number.isInteger(rule) || rule < 0 || rule > 255) {
         throw new Error('Invalid Rule');
     }
 
-    // new array updatedConfig of same size as config 
-    const n = config.length
-    const updatedConfig = new Array(n)
+    // New array updatedConfig of same size as config 
+    const n = config.length;
+    const updatedConfig = new Array(n);
     
     
+    // Apply rule to each cell and its neighbors
+    for (let i = 0; i < n; i++) {
+        const left = config[(i+n-1) % n];
+        const center = config[i];
+        const right = config[(i+1) % n];
+        const index = (left << 2) | (center << 1) | right;
+        updatedConfig[i] = (rule >> index) & 1;
+    }
+
+    // Return updated configuration
+    return updatedConfig;
+
 }
 
-const testConfig = [0,1,0,0,1,1,0,0]
-const testRule = 50
+// const testConfig = [0,1,0,0,1,1,0,0]
+// const testRule = 50
 
-applyRule(testConfig, testRule)
+// const newconfig = applyRule(testConfig, testRule)
+// console.log(newconfig)
 
-
-module.exports = { applyRule };
+// module.exports = { applyRule };
