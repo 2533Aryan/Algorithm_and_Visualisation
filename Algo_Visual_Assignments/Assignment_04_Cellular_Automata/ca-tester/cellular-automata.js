ee714e484d1a09bc416eb4cc6f24b7d9b6f71891
Aryan Raval <2533aryan@gmail.com>
2023-03-17 23:12:30 -0400

    check added

--- Algo_Visual_Assignments/Assignment_04_Cellular_Automata/ca-tester/cellular-automata.js
+++ Algo_Visual_Assignments/Assignment_04_Cellular_Automata/ca-tester/cellular-automata.js
@@ -4,12 +4,12 @@ function applyRule(config, rule) {
 
     // check if the input 'config' is an array containing only 0 or 1
     if(!Array.isArray(config) || !config.every(x => x === 0 || x === 1)) {
-        throw new console.error("Invalid Input configuration");
+        throw new console.error("Invalid Input Configuration");
     }
 
     // check if the input 'rule' is an integer in range (0,255)
     if (!Number.isInteger(rule) || rule < 0 || rule > 255) {
-        throw new Error('Invalid rule');
+        throw new Error('Invalid Rule');
     }
     
 }
