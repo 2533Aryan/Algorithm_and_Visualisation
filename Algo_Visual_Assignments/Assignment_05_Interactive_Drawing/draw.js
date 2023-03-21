da9c9ad12da5c0e127891bf7f0a443ef8d28edfa
Aryan Raval <2533aryan@gmail.com>
2023-03-21 02:20:06 -0400

    progress

--- Algo_Visual_Assignments/Assignment_05_Interactive_Drawing/draw.js
+++ Algo_Visual_Assignments/Assignment_05_Interactive_Drawing/draw.js
@@ -3,38 +3,43 @@ const drawingPanel = document.getElementById("drawingPanel");
 
 // set up event listeners for click and mouse movement
 drawingPanel.addEventListener("mousedown", startDrawing);
-drawingPanel.addEventListener("mousemove", draw);
+drawingPanel.addEventListener("mousemove", continueDrawing);
 drawingPanel.addEventListener("mouseup", endDrawing);
 
 // variable to keep track of whether the user is currently drawing or not
 let isDrawing = false;
 
-// variable to store the path element for the current drawing
-let currentPath = null;
+// variable to store the starting point of the current line
+let lineStart = null;
 
 // function to start drawing
 function startDrawing(event) {
-	// create a new path element
-	currentPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
-	// set the stroke color and width
-	currentPath.setAttribute("stroke", "black");
-	currentPath.setAttribute("stroke-width", "2");
-	// move the starting point of the path to the mouse position
-	currentPath.setAttribute("d", `M${event.offsetX},${event.offsetY}`);
-	// add the path to the svg panel
-	drawingPanel.appendChild(currentPath);
+	// set the starting point of the current line
+	lineStart = { x: event.offsetX, y: event.offsetY };
 	// set isDrawing to true
 	isDrawing = true;
 }
 
-// function to draw when the mouse is moved
-function draw(event) {
+// function to continue drawing when the mouse is moved
+function continueDrawing(event) {
 	// if the user is not currently drawing, do nothing
 	if (!isDrawing) {
 		return;
 	}
-	// append a line segment to the current path from the previous position to the current position
-	currentPath.setAttribute("d", `${currentPath.getAttribute("d")} L${event.offsetX},${event.offsetY}`);
+	// create a new line element
+	const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+	// set the stroke color and width
+	line.setAttribute("stroke", "black");
+	line.setAttribute("stroke-width", "2");
+	// set the starting and ending points of the line
+	line.setAttribute("x1", lineStart.x);
+	line.setAttribute("y1", lineStart.y);
+	line.setAttribute("x2", event.offsetX);
+	line.setAttribute("y2", event.offsetY);
+	// add the line to the svg panel
+	drawingPanel.appendChild(line);
+	// update the starting point of the current line
+	lineStart = { x: event.offsetX, y: event.offsetY };
 }
 
 // function to end drawing
@@ -42,6 +47,51 @@ function endDrawing(event) {
 	// set isDrawing to false
 	isDrawing = false;
 }
+
+// // get the svg panel
+// const drawingPanel = document.getElementById("drawingPanel");
+
+// // set up event listeners for click and mouse movement
+// drawingPanel.addEventListener("mousedown", startDrawing);
+// drawingPanel.addEventListener("mousemove", draw);
+// drawingPanel.addEventListener("mouseup", endDrawing);
+
+// // variable to keep track of whether the user is currently drawing or not
+// let isDrawing = false;
+
+// // variable to store the path element for the current drawing
+// let currentPath = null;
+
+// // function to start drawing
+// function startDrawing(event) {
+// 	// create a new path element
+// 	currentPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+// 	// set the stroke color and width
+// 	currentPath.setAttribute("stroke", "black");
+// 	currentPath.setAttribute("stroke-width", "2");
+// 	// move the starting point of the path to the mouse position
+// 	currentPath.setAttribute("d", `M${event.offsetX},${event.offsetY}`);
+// 	// add the path to the svg panel
+// 	drawingPanel.appendChild(currentPath);
+// 	// set isDrawing to true
+// 	isDrawing = true;
+// }
+
+// // function to draw when the mouse is moved
+// function draw(event) {
+// 	// if the user is not currently drawing, do nothing
+// 	if (!isDrawing) {
+// 		return;
+// 	}
+// 	// append a line segment to the current path from the previous position to the current position
+// 	currentPath.setAttribute("d", `${currentPath.getAttribute("d")} L${event.offsetX},${event.offsetY}`);
+// }
+
+// // function to end drawing
+// function endDrawing(event) {
+// 	// set isDrawing to false
+// 	isDrawing = false;
+// }
 
 // const ns = "http://www.w3.org/2000/svg";
 
