e06d9b09a80bd680682805b482d17d8f6c151b2d
Aryan Raval <2533aryan@gmail.com>
2023-03-21 02:01:32 -0400

    init

--- Algo_Visual_Assignments/Assignment_05_Interactive_Drawing/draw.js
+++ Algo_Visual_Assignments/Assignment_05_Interactive_Drawing/draw.js
@@ -7,5 +7,21 @@ function drawDot(e) {
     console.log("you drew a dot!");
 
     /* complete this */
+    const para = document.querySelector("#text-box");
+    para.innerHTML = para.innerHTML + "you clicked the box!<br/>"
 
 }
+
+
+// function drawDot(e) {
+//     let rect = box.getBoundingClientRect();
+//     let x = e.clientpX - rect.left;
+//     let y = e.clientY - rect.top;
+//     // console.log("Clicked: " + x + ", " + y);
+//     let dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+//     dot.setAttributeNS(null, "cx", x);
+//     dot.setAttributeNS(null, "cy", y);
+//     dot.classList.add("dot");
+//     box.appendChild(dot);
+    
+// }
