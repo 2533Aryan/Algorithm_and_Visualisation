fbb3c7c114e112fc8c1c44787d720373017b4958
Aryan Raval <2533aryan@gmail.com>
2023-04-07 03:14:22 -0400

    cleaning

--- Algo_Visual_Assignments/Assignment_07_Self_Similarity/script.js
+++ Algo_Visual_Assignments/Assignment_07_Self_Similarity/script.js
@@ -1,57 +1,3 @@
-// // Initial point
-// let x = 0, y = 0;
-
-// // Draw Barnsley fern
-// function draw(){
-//     // Update 20 times every frame
-//     for (let i = 0; i < 60000; i++) {
-//         update();
-//     }    
-// }
-
-// // Coordinate transformations
-// function update() {
-//     // Next value of x and y
-//     let nextX, nextY;
-//     let r = Math.random();
-
-//     // Transformations
-//     if (r < 0.01) {
-//         // Function 1 (Stem)
-//         nextX = 0;
-//         nextY = 0.16 * y;
-//     } else if (r < 0.86) {  
-//         // Function 2 (Successively smaller leaflets)
-//         nextX = 0.85 * x + 0.04 * y;
-//         nextY = -0.04 * x + 0.85 * y + 1.6;
-//     } else if (r < 0.93) {  
-//         // Function 3 (Largest left-hand leaflet)
-//         nextX = 0.20 * x - 0.26 * y;
-//         nextY = 0.23 * x + 0.22 * y + 1.6;
-//     } else {  
-//         // Function 4 (Largest right-hand leaflet)
-//         nextX = -0.15 * x + 0.28 * y;
-//         nextY = 0.26 * x + 0.24 * y + 0.44;
-//     }
-
-//     // Scaling and positioning
-//     let plotX = 300 * (x + 3) / 6;
-//     let plotY = 400 - 400 * ((y + 2) / 14);
-
-//     // Points as circle
-//     let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-//     circle.setAttributeNS(null, "cx", plotX);
-//     circle.setAttributeNS(null, "cy", plotY);
-//     circle.setAttributeNS(null, "r", "1");
-//     circle.setAttributeNS(null, "fill", "green");
-//     document.querySelector("svg").appendChild(circle);
-
-//     // Update x and y
-//     x = nextX;
-//     y = nextY;
-// }
-
-// draw()
 // Generate a single point of the Barnsley fern fractal
 function generatePoint(x, y, iterations) {
     // Base case: stop iterating after a certain number of iterations
@@ -63,24 +9,34 @@ function generatePoint(x, y, iterations) {
     let r = Math.random();
     let nextX, nextY;
 
+
+    // Transformations
     if (r < 0.01) {
+        // Function 1 (Stem)
         nextX = 0;
         nextY = 0.16 * y;
     } else if (r < 0.86) {
+        // Function 2 (Successively smaller leaflets)
         nextX = 0.85 * x + 0.04 * y;
         nextY = -0.04 * x + 0.85 * y + 1.6;
     } else if (r < 0.93) {
+        // Function 3 (Largest left-hand leaflet)
         nextX = 0.20 * x - 0.26 * y;
         nextY = 0.23 * x + 0.22 * y + 1.6;
     } else {
+        // Function 4 (Largest right-hand leaflet)
         nextX = -0.15 * x + 0.28 * y;
         nextY = 0.26 * x + 0.24 * y + 0.44;
     }
 
+
     // Draw the point on the canvas
+
+    // Scaling and positioning
     let plotX = 300 * (x + 3) / 6;
     let plotY = 400 - 400 * ((y + 2) / 14);
 
+    // Points as circle
     let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     circle.setAttributeNS(null, "cx", plotX);
     circle.setAttributeNS(null, "cy", plotY);
@@ -94,6 +50,7 @@ function generatePoint(x, y, iterations) {
 
 // Generate the entire Barnsley fern fractal using recursion
 function generateFractal(iterations) {
+    // Initial point
     let x = 0, y = 0;
 
     for (let i = 0; i < iterations; i++) {
@@ -102,4 +59,4 @@ function generateFractal(iterations) {
 }
 
 // Call the generateFractal function to generate the Barnsley fern fractal with 200 iterations
-generateFractal(200);
+// generateFractal(200);
