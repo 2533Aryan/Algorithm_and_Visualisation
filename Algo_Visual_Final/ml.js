86bfe2bbc30f02f14600b9f0dbdc5b09edd57135
Aryan Raval <2533aryan@gmail.com>
2023-04-30 16:30:11 -0400

    comment

--- Algo_Visual_Final/ml.js
+++ Algo_Visual_Final/ml.js
@@ -1,7 +1,8 @@
+// Define display dimensions and options
 const displayWidth = window.innerWidth - 25;
 const displayHeight = 450;
 const dataSetSize = 250;
-const options = {
+const visualizationOptions = {
   rootNode: '#knn',
   width: displayWidth,
   height: displayHeight,
@@ -9,19 +10,31 @@ const options = {
   circleFill: '#3fe4h2',
   circleStroke: 'white' 
 };
-const types = ['A', 'B'];
 
+// Define types of data points
+const dataPointTypes = ['A', 'B'];
+
+// Generate a random integer between min and max values
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// Generate random coordinates for an ellipsoid shape
 function createRandomEllipsoidCoordinates(width, height, cx, cy) {
-  const rho = Math.sqrt(Math.random()) 
-  const phi = Math.random() * Math.PI * 2
-  const rands = {x: getRandomInt(-width/2,width/2), y: getRandomInt(-height/2,height/2)}
-  const x = (rho * Math.cos(phi) * width/2) + cx + rands.x
-  const y = (rho * Math.sin(phi) * height/2) + cy + rands.y
-  return {x, y}
+  // Generate random values for rho and phi angles
+  const rho = Math.sqrt(Math.random()); 
+  const phi = Math.random() * Math.PI * 2;
+  
+  // Generate random x and y values within the specified width and height
+  const xRandomOffset = getRandomInt(-width/2, width/2);
+  const yRandomOffset = getRandomInt(-height/2, height/2);
+  const x = (rho * Math.cos(phi) * width/2) + cx + xRandomOffset;
+  const y = (rho * Math.sin(phi) * height/2) + cy + yRandomOffset;
+  
+  return {x, y};
 }
+
+// Generate a dataset with random coordinates for each type of data point
 function createRandomData() {
   const ellipsoidOptions = {
     'A': {
@@ -37,16 +50,20 @@ function createRandomData() {
        cy: displayHeight*0.66
     }
   };
-  return Array.apply(null, Array(dataSetSize))
-    .map(d => {
-      const type = Math.random() > 0.5 ? types[0] : types[1];
-      const {width, height, cx, cy} = ellipsoidOptions[type]
-      const {x, y} = createRandomEllipsoidCoordinates(width, height, cx, cy);
-      return {x, y, type};
-    }
-  );
+  
+  // Generate random data points for each type and return combined dataset
+  const data = Array.from({length: dataSetSize}, () => {
+    const type = Math.random() > 0.5 ? dataPointTypes[0] : dataPointTypes[1];
+    const {width, height, cx, cy} = ellipsoidOptions[type];
+    const {x, y} = createRandomEllipsoidCoordinates(width, height, cx, cy);
+    return {x, y, type};
+  });
+  
+  return data;
 }
+
+// Generate random data and visualize with k-NN algorithm
 const data = createRandomData();
 const k = 3;
-const vis = new d3ml.KNNVisualization(data, options, types, k);
-vis.draw(); 
+const visualization = new d3ml.KNNVisualization(data, visualizationOptions, dataPointTypes, k);
+visualization.draw();
